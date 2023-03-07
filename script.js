2b3daf05e0205c3ed5e74cd3d7a5fa033714f42d
oibasqada <tumarbekovmax@gmail.com>
2023-03-08 00:28:38 +0600

    Done

--- script.js
+++ script.js
@@ -2,17 +2,23 @@
 // let b = 'Ivan'
 //
 // console.log ((a.replace('h', 'H') ), (b.replace('I','i')))
+// Hello ivan
+
 
 // let a = 'lorem ipsum dolor'
 // let b = a.length
 //
 // console.log(b)
+// записать длину в переменную b
+
 
 // let a = 'lorem ipsum dolor sit amet'
 // let b = a.replace('sit ', '')
 // let text = 'sit'+ ' ' + b
-
+//
 // console.log (text)
+// вернуть sit lorem ipsum dolor amet
+
 
 // SOLUTION BENEATH IS DONE BY THE TOPIC OF SECOND LESSON "JS"
 
@@ -27,26 +33,67 @@
 // let s = a.slice(0,7).toUpperCase() + 'n'
 //
 // console.log(s)
+//верунть CHILDREn
 
 
 // let a = 'boss'
 // let text = a.replace('boss', 'bsso')
 //
 // console.log(text)
+// верунть bsso
 
 
 // let a = 'sunday'
 // let b = a.indexOf('d')
 //
 // console.log(b)
+// вернуть index d
 
 
 // let a = 'hello'
 // let b = a.replace('ll', 'ks')
 //
 // console.log(b)
+// верунть hekso
 
 
 // let a = 'user'
 // let b = 'new'
+// let text = b + ' ' + a.slice(0,2)
+//
+// console.log(text)
 // верунть new us
+
+
+// let a = 'monday'
+// let b = 'day'
+// let text = b + a.slice(3,6) + a.slice(0,3)
+//
+// console.log(text)
+// вернуть daydaymon
+
+
+// let a = 'HELLO'
+// let text = a.replace('HELLO','hElLo')
+//
+// console.log(text)
+// вернуть 'hElLo'
+
+
+// let a = 5
+// let b = '400'
+// let s = a * 10 + Number(b)
+// let c = s.toString()
+//
+// console.log(c)
+// вернуть '450'
+
+
+// let a = 'monkey'
+// let b = 'banana'
+// let c = b.slice(2, 6).replace('n', 'm') + 'k'
+//
+// console.log(c)
+// вернуть manak
+
+
