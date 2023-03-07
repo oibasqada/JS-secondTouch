31ede166d13d19d14fbe9fc4261f2ccd8bc3b854
oibasqada <tumarbekovmax@gmail.com>
2023-03-07 23:55:27 +0600

    lesson 2 js

---
+++ script.js
@@ -0,0 +1,52 @@
+// let a = 'hello'
+// let b = 'Ivan'
+//
+// console.log ((a.replace('h', 'H') ), (b.replace('I','i')))
+
+// let a = 'lorem ipsum dolor'
+// let b = a.length
+//
+// console.log(b)
+
+// let a = 'lorem ipsum dolor sit amet'
+// let b = a.replace('sit ', '')
+// let text = 'sit'+ ' ' + b
+
+// console.log (text)
+
+// SOLUTION BENEATH IS DONE BY THE TOPIC OF SECOND LESSON "JS"
+
+// let a = 'children'
+// let text = a.replace('childre', 'CHILDRE')
+//
+// console.log (text)
+
+// NEXT COMING SOLUTION DONE OUT OF TOPIC, JUST TRYING OTHER WAYS OF MAKING LETTERS UPPERCASE
+
+// let a = 'children';
+// let s = a.slice(0,7).toUpperCase() + 'n'
+//
+// console.log(s)
+
+
+// let a = 'boss'
+// let text = a.replace('boss', 'bsso')
+//
+// console.log(text)
+
+
+// let a = 'sunday'
+// let b = a.indexOf('d')
+//
+// console.log(b)
+
+
+// let a = 'hello'
+// let b = a.replace('ll', 'ks')
+//
+// console.log(b)
+
+
+// let a = 'user'
+// let b = 'new'
+// верунть new us
